53ede6f332cbb01e1cfaa1b65ef05f8deb7da408
Tom Bredemeier <tbredemeier@barrington220.org>
2018-06-21 11:52:54 -0500

    Connected UI objects to storyboard

--- Assignment Notebook/DetailViewController.swift
+++ Assignment Notebook/DetailViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var subjectTextField: UITextField!
+    @IBOutlet weak var dueDateTextField: UITextField!
+    @IBOutlet weak var detailTextField: UITextField!
+    var detailItem: Assignment? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
 
     func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
+        // Update the user interface for the detail item
+        if let assignment = self.detailItem {
+            if titleTextField != nil {
+                titleTextField.text = assignment.title
+                subjectTextField.text = assignment.subject
+                dueDateTextField.text = assignment.dueDate
+                detailTextField.text = assignment.detail
             }
         }
     }
@@ -33,13 +44,14 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    var detailItem: NSDate? {
-        didSet {
-            // Update the view.
-            configureView()
+    override func viewWillDisappear(_ animated: Bool) {
+        if let assignment = self.detailItem {
+            assignment.title = titleTextField.text!
+            assignment.subject = subjectTextField.text!
+            assignment.dueDate = dueDateTextField.text!
+            assignment.detail = detailTextField.text!
         }
     }
-
 
 }
 
